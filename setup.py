8fb4c522a01ddedff34b469378c742d345336751
Yu Gui <yugui923@gmail.com>
2017-03-28 19:36:30 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='IsoPass',
-    version='0.1.0',
+    version='0.1.1',
     author='Yu Gui',
     author_email='yugui923@gmail.com',
     packages=['isopass'],
